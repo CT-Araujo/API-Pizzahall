59a5f9109876a66a867e82a9c7416928dda17ef8
Cleiton Araujo <cleitonaraujo1311@gmail.com>
2024-06-19 11:23:04 -0300

    Retorno do svg

--- app/transactions.py
+++ app/transactions.py
@@ -1,38 +1,23 @@
+
+import qrcode.image.svg
 from django.http import HttpResponse
 from pixqrcode import PixQrCode
 import base64
-import io
 
 def PagamentoPix(nome, chave, cidade, valor):
     pix = PixQrCode(str(nome), str(chave), str(cidade), str(valor))
     payload = pix.generate_code()
-        
+
     import qrcode
     import qrcode.image.svg
 
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(payload)
-    qr.make(fit=True)
+    factory = qrcode.image.svg.SvgPathImage
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    img_bytes = io.BytesIO()
-    img.save(img_bytes, format='PNG')
-    img_bytes = img_bytes.getvalue()
-
-    encoded_img = base64.b64encode(img_bytes).decode('utf-8')
-    decoded_img_bytes = base64.b64decode(encoded_img)
-
-    img_bytes = io.BytesIO(decoded_img_bytes)
-
-
-
+    img = qrcode.make(str(payload), image_factory=factory)
+    stringImage = img.to_string(encoding='unicode')
+    encoded_svg = base64.b64encode(stringImage.encode('utf-8')).decode('utf-8')
     
-    return {'payload': payload, 'png': encoded_img,"nome": nome}
+    return {'payload': payload, 'png': encoded_svg,"nome": nome}
     
     
 
